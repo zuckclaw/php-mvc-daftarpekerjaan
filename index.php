d7bd3484264d7a9e53f7145796dce5773907b100
Fathin <fathinjamaluddin666@gmail.com>
2025-09-09 07:40:06 +0700

    add indexphp, include file

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+include "connection.php";
+include "model/pekerjaan.model.php";
+include "controller/pekerjaan.controller.php";
+include "view/pekerjaan/pekerjaan.view.php";
+
+$pekerjaan = new PekerjaanView();
+?>
