8da886c6cac5ed1f8fc54d578eadfafdf5575319
Fathin <fathinjamaluddin666@gmail.com>
2025-09-09 07:46:58 +0700

    add html

--- index.php
+++ index.php
@@ -6,3 +6,28 @@ include "view/pekerjaan/pekerjaan.view.php";
 
 $pekerjaan = new PekerjaanView();
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pekerjaan</title>
+</head>
+<body>
+    <?php include "view/utils/pekerjaan.utils.php"; ?>
+    <?php $pekerjaan->find(); ?>
+
+    <table border="1">
+        <tr>
+            <th>ID</th>
+            <th>Lowongan</th>
+            <th>Deskripsi</th>
+            <th>Perushaan</th>
+        </tr>
+        <?php
+        $pekerjaan->show();
+        ?>
+    </table>
+</body>
+</html>
