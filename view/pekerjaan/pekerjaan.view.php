c363ad9dcf830e7133083370fcfaf26e8a8fb5c0
Fathin <fathinjamaluddin666@gmail.com>
2025-09-09 07:34:30 +0700

    add method show

--- view/pekerjaan/pekerjaan.view.php
+++ view/pekerjaan/pekerjaan.view.php
@@ -10,6 +10,29 @@ class PekerjaanView extends PekerjaanController
         $this->pekerjaans = $this->getPekerjaan();
         $this->pekerjaan = $this->getPekerjaanBy();
     }
+
+    public function show()
+    {
+        if (isset($this->pekerjaan)) {
+            foreach ($this->pekerjaan as $item) {
+                echo "<tr>";
+                echo "<td>" . $item['id'] . "</td>";
+                echo "<td>" . $item['lowongan'] . "</td>";
+                echo "<td>" . $item['deskripsi'] . "</td>";
+                echo "<td>" . $item['perusahaan'] . "</td>";
+                echo "</tr>";
+            }
+        } else {
+            foreach ($this->pekerjaans as $item) {
+                echo "<tr>";
+                echo "<td>" . $item['id'] . "</td>";
+                echo "<td>" . $item['lowongan'] . "</td>";
+                echo "<td>" . $item['deskripsi'] . "</td>";
+                echo "<td>" . $item['perusahaan'] . "</td>";
+                echo "</tr>";
+            }
+        }
+    }
 }
 
 ?>
