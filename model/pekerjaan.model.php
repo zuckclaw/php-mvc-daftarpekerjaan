e1553ab48d5f449951ff17ff9f357aa1ac2c55d0
Fathin <fathinjamaluddin666@gmail.com>
2025-09-09 07:17:41 +0700

    add model

---
+++ model/pekerjaan.model.php
@@ -0,0 +1,19 @@
+<?php
+
+class PekerjaanModel extends Connection {
+    protected function findAll(){
+        $sql = "SELECT * FROM pekerjaan";
+        $result = $this->connect()->query($sql);
+        if ($result->num_rows > 0) {
+            while ($data = mysqli_fetch_assoc($result)) {
+                $pekerjaan[] = $data;
+            }
+        }
+        return $pekerjaan;
+    }
+
+    
+
+}
+
+?>
